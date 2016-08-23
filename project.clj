f2275bfbdd02f67b58887fec0ef6f9b6983c3c06
Greg Winn <gregw@mayvenn.com>
2016-08-23 15:12:19 -0700

    Version 0.1.9-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject limo "0.1.8"
+(defproject limo "0.1.9-SNAPSHOT"
   :description "A clojure wrapper around selenium webdriver"
   :url "https://github.com/mayvenn/limo"
   :license {:name "Eclipse Public License"
