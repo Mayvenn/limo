6691c7b5b0f6405c84f3e7d7f6d9e0b1efdb5b19
Burin Choomnuan <agilecreativity@gmail.com>
2017-07-30 13:57:06 -0400

    Add more support for by/* locators
    - Add by/tag-name, by/link-text, by/partial-link-text, etc
    - Add basic unit tests

--- test/limo/api_test.clj
+++ test/limo/api_test.clj
@@ -1,10 +1,32 @@
 (ns limo.api-test
   (:require [clojure.test :refer :all]
             [limo.api :refer :all]
-            [limo.driver :refer :all]))
+            [limo.driver :refer :all]
+            [limo.api :as api]))
 
 (deftest opening-a-browser
   (with-fresh-browser create-chrome
-    (to "http://httpbin.org")
+    (to "https://httpbin.org")
     (click "a[href='/html']")
     (is (text= "h1" "Herman Melville - Moby-Dick"))))
+
+(deftest test-various-by-locators
+  (with-fresh-browser create-chrome
+    (to "https://httpbin.org")
+    (click-when-visible {:partial-link-text "anything"})
+    (is (= (current-url) "https://httpbin.org/anything"))
+
+    (to "https://httpbin.org")
+    ;; Note: click on link with label 'now.httpbin.org'
+    (click-when-visible {:xpath "//*[@id=\"manpage\"]/div/ul[1]/li/a/code"})
+    (is (= (current-url) "https://now.httpbin.org/"))
+
+    (to "https://httpbin.org")
+    ;; Note: click on the non secure link using :css-selector locator
+    (click-when-visible {:css-selector "#manpage > div > p:nth-child(2) > a:nth-child(1)"})
+    (is (= (current-url) "http://httpbin.org/"))
+
+    (to "https://httpbin.org")
+    ;; Note: click on the secure link using :css-selector locator
+    (click-when-visible {:css-selector "#manpage > div > p:nth-child(2) > a:nth-child(2)"})
+    (is (= (current-url) "https://httpbin.org/"))))
