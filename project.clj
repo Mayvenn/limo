00708694f9f0da41150420033604b4465a3f5e23
Burin Choomnuan <agilecreativity@gmail.com>
2017-07-30 13:32:54 -0400

    Use recent vesion of the dependencies
    - Upgrade Clojure to 1.8.0
    - Upgrade selenium-* to 3.4.0
    - Upgrade other libs to the most recent version

--- project.clj
+++ project.clj
@@ -3,15 +3,15 @@
   :url "https://github.com/mayvenn/limo"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.clojure/tools.logging "0.3.1"]
-                 [org.seleniumhq.selenium/selenium-server "2.47.1"]
-                 [org.seleniumhq.selenium/selenium-java "2.47.0"]
-                 [environ "1.0.0"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/tools.logging "0.4.0"]
+                 [org.seleniumhq.selenium/selenium-server "3.4.0"]
+                 [org.seleniumhq.selenium/selenium-java "3.4.0"]
+                 [environ "1.1.0"]]
   :profiles
   {:dev {:source-paths ["dev"]
-         :dependencies [[pjstadig/humane-test-output "0.6.0"]
+         :dependencies [[pjstadig/humane-test-output "0.8.2"]
                         [org.clojure/tools.namespace "0.2.11"]]
-         :plugins [[lein-cljfmt "0.3.0"]]
+         :plugins [[lein-cljfmt "0.5.6"]]
          :injections [(require 'pjstadig.humane-test-output)
                       (pjstadig.humane-test-output/activate!)]}})
