9bb010935f9951eadeef00985a5d88d4824ca208
Jeff Hui <jeff@mayvenn.com>
2019-11-18 18:11:49 -0800

    Version 0.2.6-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject limo "0.2.5"
+(defproject limo "0.2.6-SNAPSHOT"
   :description "A clojure wrapper around selenium webdriver"
   :url "https://github.com/mayvenn/limo"
   :license {:name "Eclipse Public License"
